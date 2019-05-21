2d12977d571525849b737c24ef524f29645c14e2
Oleksii Nadtochii <a.nadtochey@elmy.com>
2019-05-21 17:47:18 +0300

    Initial application version based on maven quickstart

---
+++ src/main/java/com/github/irbis/games/rps/App.java
@@ -0,0 +1,13 @@
+package com.github.irbis.games.rps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
